2e251fc994a7191b63e5ff7613c23790a107accf
liupingting <1483705281@qq.com>
2016-11-04 01:33:50 +0800

    new

--- app/wangpan/wangpan.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'wangpan',
-    templateUrl: 'app/wangpan/wangpan.component.html',
-    styleUrls: ['app/wangpan/wangpan.component.css']
-})
-export class WangpanComponent { }
