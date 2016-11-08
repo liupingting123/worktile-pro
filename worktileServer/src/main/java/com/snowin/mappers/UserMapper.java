d5c9b40ff375a6245d2f3cff87c9e08929357514
Snowin1994 <Snowin1994@foxmail.com>
2016-11-08 13:49:33 +0800

    basic code Success, jdbc version problem

--- worktileServer/src/main/java/com/snowin/mappers/UserMapper.java
+++ worktileServer/src/main/java/com/snowin/mappers/UserMapper.java
@@ -2,12 +2,14 @@ package com.snowin.mappers;
 
 
 import com.snowin.model.User;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Snowin on 2016/11/5 0005.
  */
 
-
+//@Repository(value="userMapper")
 public interface UserMapper {
-    User selectUser(String username);
+    User selectUser(/*@Param("username")*/ String username);
 }
--- worktileServer/src/main/java/com/snowin/service/impl/UserServiceImpl.java
+++ worktileServer/src/main/java/com/snowin/service/impl/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package com.snowin.service.impl;
 
-import com.snowin.model.User;
 import com.snowin.mappers.UserMapper;
+import com.snowin.model.User;
 
 import com.snowin.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +15,13 @@ import org.springframework.stereotype.Service;
 @Service("userService")
 public class UserServiceImpl implements IUserService {
 
-//    @Autowired
+    @Autowired
     private UserMapper userMapper;
 
-    @Override
     public User getUserByName(String username) {
 
         return userMapper.selectUser(username);
     }
+
+
 }
